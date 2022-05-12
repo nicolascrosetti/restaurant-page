e7adbbf1055bae047bc42c12235c491bca20fdde
Nico <nccrosetti@gmail.com>
2022-05-12 10:14:20 -0300

    Create initial files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const content = document.querySelector('#content');
+//Restaurant title
+const title = document.createElement('h1');
+title.textContent = 'Restaurant';
+content.append(title);
+//Image
+const image = document.createElement('img');
+image.src = "../images/restaurant.jpg";
+content.append(image);
+//Description
+const description = document.createElement('p');
+description.textContent = 'Something about a restaurant!, come to our restaurant, the best restaurant in town, etc.'
+content.append(description);
+
