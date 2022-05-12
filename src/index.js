eb1387200efdd769b5cc6b12c9797d0cd98f0982
Nico <nccrosetti@gmail.com>
2022-05-12 10:30:06 -0300

    Add page-load module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-const content = document.querySelector('#content');
-//Restaurant title
-const title = document.createElement('h1');
-title.textContent = 'Restaurant';
-content.append(title);
-//Image
-const image = document.createElement('img');
-image.src = "../images/restaurant.jpg";
-content.append(image);
-//Description
-const description = document.createElement('p');
-description.textContent = 'Something about a restaurant!, come to our restaurant, the best restaurant in town, etc.'
-content.append(description);
+import { pageLoad } from "./page-load";
+
+pageLoad();
 
